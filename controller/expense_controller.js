7626319377812c7c3670891c128c2da60932683a
aaronlhma <aaronlhma@gmail.com>
2021-11-21 17:08:41 -0800

    listing works

--- controller/expense_controller.js
+++ controller/expense_controller.js
@@ -1,18 +1,30 @@
-const database = require("../database.js")
-static_list = [{ id: 1, date: "2021-05-03", transaction: "Bought an apple", cost: 5, tags:'test' }]
+const database = require("../database.js");
+static_list = [{ id: 1, date: "2021-05-03", transaction: "Bought an apple", cost: 5, tags:'test' }];
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'app'
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log('connected');
+})
 
 let expenseController = {
     list: async(req, res) => {
-        res.render("expense/index",{expenses: static_list});
-        // let expenseToFind = req.params.id;
-        // let searchResult = req.user.expenses.find(function(expense) {
-        //     return expense.id == expenseToFind;
-        // });
-        // if (searchResult != undefined) {
-        //     res.render("expense/single-expense", { expenseItem: searchResult });
-        // } else {
-        //     res.render("expense/index", { expenses: req.user.expenses });
-        // }
+        sql = `
+        SELECT * from expenses;
+        `
+        con.query(sql, function(err, result) {
+            if(err) throw err;
+            console.log(result)
+            res.render("expense/index",{expenses: result})
+        })
+
     },
 
     new: (req, res) => {
@@ -20,43 +32,43 @@ let expenseController = {
     },
 
     listOne: async(req, res) => {
-        console.log(static_list[req.params.id-1])
         try {
-            res.render("expense/single-expense",{expenseItem:static_list[req.params.id-1]})
+            console.log('PARAMS ID IS :' + req.params.id)
+
+            sql = `
+            SELECT * from expenses WHERE id = ${req.params.id}
+            `
+            con.query(sql, function(err,result) {
+                if(err) throw err;
+                console.log(result)
+                res.render("expense/single-expense",{ expenseItem: result[0] })
+            })
+
         }
         catch {
-            res.render("expense/index",{expenses: static_list})
+            sql = `
+            SELECT * from expenses;
+            `
+            con.query(sql, function(err, result) {
+                if(err) throw err;
+                console.log(result)
+                res.render("expense/index",{expenses: result})
+            })
         }
-        // let expenseToFind = req.params.id;
-        // let searchResult = req.user.expenses.find(function(expense) {
-        //     return expense.id == expenseToFind;
-        // });
-        // if (searchResult != undefined) {
-        //     res.render("expense/single-expense", { expenseItem: searchResult });
-        // } else {
-        //     res.render("expense/index", { expenses: req.user.expenses });
-        // }
+
     },
 
     create: async(req, res) => {
-    
-        // let id_list = [];
-        // for (item of req.user.expenses) {
-        //     id_list.push(item.id)
-        // }
-        // const set = new Set(id_list);
-        // let id = 1;
-        // while (set.has(id)) {
-        //     id++
-        // }
-        // let expense = {
-        //     id: id,
-        //     date: req.body.datetime,
-        //     transaction: req.body.transaction,
-        //     cost: req.body.cost
-        // };
-        // req.user.expenses.push(expense);
-        res.redirect("/expenses");
+
+        sql = `
+        INSERT INTO expenses
+        VALUES(${req.body.transaction},${req.body.cost},${req.body.tags},${req.user.id})
+        `
+        con.query(sql, function(err,result) {
+            if(err) throw err;
+            console.log(result)
+            res.redirect("/expenses");
+        })
     },
 
     edit: async(req, res) => {
@@ -74,16 +86,7 @@ let expenseController = {
     },
 
     update: async(req, res) => {
-        // let reminderToUpdate = req.params.id;
-        // let { datetime, transaction, cost } = req.body;
-        // for (var r in req.user.expenses) {
-        //     if (req.user.expenses[r].id.toString() === reminderToUpdate) {
-        //         req.user.expenses[r].transaction = transaction;
-        //         req.user.expenses[r].cost = cost;
-        //         req.user.expenses[r].date = datetime;
-        //         break;
-        //     }
-        // }
+
         res.redirect("/expenses");
     },
 
